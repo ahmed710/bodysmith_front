004fa13c2745f4a1d2f64135ce09ae3e0217796a
ahmed <ahmedboukha126@gmail.com>
2024-06-24 16:35:43 +0100

    after_adding_form

--- src/app/modules/admin/dashboards/add-reclamation/add-reclamation.component.ts
+++ src/app/modules/admin/dashboards/add-reclamation/add-reclamation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AddReclamationService} from "./add-reclamation.service";
 
 @Component({
   selector: 'app-add-reclamation',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-reclamation.component.scss']
 })
 export class AddReclamationComponent {
+    constructor(private consumer:AddReclamationService){}
+    reclamation: FormGroup = new FormGroup({
+        title: new FormControl('', [Validators.required]),
+        description: new FormControl(''),
+        email: new FormControl('', [Validators.required]),
+        numTelReclamation: new FormControl(0, [
+            Validators.required,
+            Validators.pattern('[0-9]+'),
+        ]),
+        pieceJointe: new FormControl('' ),
+        typeReclamation: new FormControl(0 ),
+        userReclamation: new FormControl(0),
+    });
 
+    add() {
+        this.consumer.addReclamation(this.reclamation.value).subscribe({
+            next: ()=> alert('Added successfuly')
+        })
+    }
 }
